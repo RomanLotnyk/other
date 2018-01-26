9b727d91ff5f9fd9a9ea4fbf8b15b4899db9c070
RomanLotnyk <lotnikroman@yandex.ru>
2018-01-26 17:38:48 +0400

    ip

--- ip/src/main/java/ip/IPDiapason.java
+++ ip/src/main/java/ip/IPDiapason.java
@@ -1,7 +1,18 @@
 package ip;
 
-/**
- * Created by PK on 25.01.2018.
- */
+
 public class IPDiapason {
+
+
+    public int convertToInt(char ip[]) {
+        return ((ip[0] * 256 + ip[1]) * 256 + ip[2]) * 256 + ip[3];
+    }
+
+    public  String convertToString(long i) {
+        return  ((i >> 24 ) & 0xFF) + "." +
+                ((i >> 16 ) & 0xFF) + "." +
+                ((i >>  8 ) & 0xFF) + "." +
+                (i & 0xFF);
+    }
+
 }
--- ip/src/test/java/ip/IPDiapasonTest.java
+++ ip/src/test/java/ip/IPDiapasonTest.java
@@ -1,8 +1,51 @@
+package ip;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-/**
- * Created by PK on 25.01.2018.
- */
 public class IPDiapasonTest {
 
+    @Test
+    public void whenReturnAssertThatDiapason() {
+
+        String first = "192.168.0.1";
+        String second = "192.168.0.5";
+
+        String[] fOne = first.split("[.]");
+        String[] fTwo = second.split("[.]");
+
+        char[] firstIp = new char[4];
+        char[] secondIp = new char[4];
+
+        firstIp[0] = (char) Integer.parseInt(fOne[0]);
+        firstIp[1] = (char) Integer.parseInt(fOne[1]);
+        firstIp[2] = (char) Integer.parseInt(fOne[2]);
+        firstIp[3] = (char) Integer.parseInt(fOne[3]);
+
+        secondIp[0] = (char) Integer.parseInt(fTwo[0]);
+        secondIp[1] = (char) Integer.parseInt(fTwo[1]);
+        secondIp[2] = (char) Integer.parseInt(fTwo[2]);
+        secondIp[3] = (char) Integer.parseInt(fTwo[3]);
+
+
+
+        IPDiapason ip = new IPDiapason();
+
+        long one = ip.convertToInt(firstIp);
+        long two = ip.convertToInt(secondIp);
+
+        StringBuilder result = new StringBuilder();
+
+        for (long i = one + 1; i < two; i++) {
+            String res = ip.convertToString(i);
+            result.append(res + "\n");
+        }
+
+        StringBuilder diapason = new StringBuilder();
+        diapason.append("192.168.0.2\n");
+        diapason.append("192.168.0.3\n");
+        diapason.append("192.168.0.4\n");
+        assertThat(result.toString(), is(diapason.toString()));
+    }
 }
