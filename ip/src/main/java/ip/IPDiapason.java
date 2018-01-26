9e5c4323b053a111eac1ecaefc307a100686fd16
RomanLotnyk <lotnikroman@yandex.ru>
2018-01-26 17:36:55 +0400

    update

---
+++ ip/src/main/java/ip/IPDiapason.java
@@ -0,0 +1,7 @@
+package ip;
+
+/**
+ * Created by PK on 25.01.2018.
+ */
+public class IPDiapason {
+}
---
+++ ip/src/test/java/ip/IPDiapasonTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by PK on 25.01.2018.
+ */
+public class IPDiapasonTest {
+
+}
