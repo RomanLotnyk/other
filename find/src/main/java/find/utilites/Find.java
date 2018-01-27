c7919052e5c351f614df7b6fb3c146a59a534f21
RomanLotnyk <lotnikroman@yandex.ru>
2018-01-28 03:04:09 +0400

    find

--- find/src/main/java/find/utilites/Find.java
+++ find/src/main/java/find/utilites/Find.java
@@ -6,30 +6,24 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Find {
-
+    /**
+     * data.
+     */
     private HashMap<String, Contact> data;
 
+    /**
+     * Constructor.
+     * @param data data
+     */
     public Find(HashMap<String, Contact> data) {
        this.data = data;
     }
 
     /**
-     * find.
-     * @param name
-     * @return
+     * search.
+     * @param name name
+     * @return return
      */
-    public List<String> find(String name) {
-        List<String> numbers = new ArrayList<>();
-        if (this.data.containsKey(name)) {
-            for (String number: this.data.get(name).getNumbers()) {
-                numbers.add(number);
-            }
-        } else {
-            System.err.println("Contact Not Found!");
-        }
-        return numbers;
-    }
-
     public List<String> search(String name) {
         List<String> numbers = new ArrayList<>();
         numbers.addAll(this.data.getOrDefault(name, new Contact(name, numbers)).getNumbers());
@@ -37,36 +31,5 @@ public class Find {
             System.err.println("Contact Not Found!");
         }
         return numbers;
-    }
-
-    public static void main(String[] args) {
-        List<String> ivanNumbers = new ArrayList<>();
-        ivanNumbers.add("+8 800 2000 500");
-        ivanNumbers.add("+8 800 200 600");
-
-        List<String> sidNumbers = new ArrayList<>();
-        sidNumbers.add("+8 800 2000 800");
-        sidNumbers.add("+8 800 2000 900");
-        sidNumbers.add("+8 800 2000 000");
-
-        List<String> petNumbers = new ArrayList<>();
-        petNumbers.add("+8 800 2000 700");
-
-
-        Contact ivan = new Contact("Иванов И.И.", ivanNumbers);
-        Contact sid = new Contact("Сидоров С.С.", sidNumbers);
-        Contact pet = new Contact("Петров П.П.", petNumbers);
-        HashMap<String, Contact> data = new HashMap<>();
-        data.put("Иванов И.И.", ivan);
-        data.put("Сидоров С.С.", sid);
-        data.put("Петров П.П.", pet);
-
-        Find find = new Find(data);
-        List<String> res = find.search("Петров П.П.");
-
-        for (String numbers : res) {
-            System.out.println(numbers);
-        }
-
     }
 }
